c9ebc7b4d0423108ed901c8eedec3ba987751590
Privezenov96 <91391340+Privezenov96@users.noreply.github.com>
2021-12-03 12:55:14 +0300

    24.11

--- src/ru/mirea/task10/factorization/factorization.java
+++ src/ru/mirea/task10/factorization/factorization.java
@@ -3,5 +3,37 @@ package ru.mirea.task10.factorization;
 import java.util.Scanner;
 
 public class factorization {
-    Scanner in = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите число: ");
+        int digit = sc.nextInt();
+
+        int temp = digit;
+        String out = "";
+        for (int i=2; i<=digit; i++){
+            while(temp%i==0 && simple_digit_ans(i)){
+                temp/=i;
+                out += Integer.toString(i) + " ";
+            }
+        }
+        System.out.println(out);
+    }
+    static boolean simple_digit_ans(int digit){
+        if (digit==1){
+            return true;
+        }
+        return simple_digit_ans(digit, digit-1);
+    }
+
+    public static boolean simple_digit_ans(int digit, int div){
+        if (div==1){
+            return true;
+        }
+        if (digit%div==0){
+            return false;
+        }
+        else{
+            return simple_digit_ans(digit, div-1);
+        }
+    }
 }
