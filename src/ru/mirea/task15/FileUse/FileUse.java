ae79ce13f246092f400276a9c3c060d113ce6650
Privezenov96 <91391340+Privezenov96@users.noreply.github.com>
2021-12-03 13:03:13 +0300

    24.11

--- src/ru/mirea/task15/FileUse/FileUse.java
+++ src/ru/mirea/task15/FileUse/FileUse.java
@@ -1,26 +1,59 @@
 package ru.mirea.task15.FileUse;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
-public class FileUse  {
-    FileUse() {
-        // System.out.println(f.isFile());
-        try(FileWriter writer = new FileWriter("File.txt", false))
-        {
-            System.out.println("Введите строку");
-            Scanner in = new Scanner(System.in);
-            String text = in.nextLine();
-            writer.write(text);
-            writer.flush();
-        }
-        catch(IOException ex){
+public class FileUse {
+    public static void main(String[] args) {
+        String path = "src/ru/mirea/task15/FileUse/File.txt";
+        //1 -- записать в файл с клавиатуры
+        System.out.println("Введите что-нибудь");
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        writeFile(path, str,false);
+        //2 -- вывод из файла
+        System.out.print("Было введено: ");
+        readFile(path);
+        //3 -- замена в текста в файле на текст из клавиатуры
+        System.out.println("Напишите еще что-нибудь");
+        str = scanner.nextLine();
+        writeFile(path, str, false);
+        //4 -- добавление в конец файла текста, введенного с клавиатуры
+        System.out.println("Напишите еще что-нибудь");
+        str = scanner.nextLine();
+        writeFile(path, str, true);
+    }
 
+    static void readFile(String name){
+        //чтение из файла
+        try(FileReader reader = new FileReader(name))
+        {
+            int c;
+            while((c=reader.read())!=-1){
+                System.out.print((char)c);
+            }
+            System.out.println();
+        }
+        catch(IOException ex)
+        {
             System.out.println(ex.getMessage());
         }
     }
-    public static void main(String[] args)
-    {
-        FileUse a = new FileUse();
+
+    static void writeFile(String name, String str, boolean append){
+        //запись в файл
+        try(FileWriter writer = new FileWriter(name, append))
+        {
+            writer.write(str);
+            writer.append("\n");
+            writer.flush();
+        }
+        catch(IOException ex)
+        {
+            System.out.println(ex.getMessage());
+        }
     }
 }
