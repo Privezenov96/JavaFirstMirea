7f81f48484c98d9c6a13cc36c0e22eafc2ed18b7
Privezenov96 <91391340+Privezenov96@users.noreply.github.com>
2021-12-22 12:15:07 +0300

    21

---
+++ src/ru/mirea/task21/Task21.java
@@ -0,0 +1,9 @@
+package ru.mirea.task21;
+
+public class Task21 {
+    public static void main(String[] args) {
+        SubwayLines subwayLines = new SubwayLines();
+
+        subwayLines.printLines(false);
+    }
+}
