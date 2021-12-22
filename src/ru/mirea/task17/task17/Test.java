9cc4db719ba3b85f69958f88cdb2a44a7f083a20
Privezenov96 <91391340+Privezenov96@users.noreply.github.com>
2021-12-22 11:52:22 +0300

    24.11

---
+++ src/ru/mirea/task17/task17/Test.java
@@ -0,0 +1,23 @@
+package ru.mirea.task17.task17;
+
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int planetNum;
+
+        System.out.print("Enter planet number (from 0): ");
+
+        planetNum = sc.nextInt();
+
+        if (planetNum < 8) {
+            System.out.print("Force of gravity: ");
+            System.out.print(Planet.values()[planetNum].getP());
+            System.out.print(" m/s²");
+        }
+        else {
+            System.out.println("Error. No planet with this number.");
+        }
+    }
+}
