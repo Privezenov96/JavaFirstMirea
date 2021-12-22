378f90a0aaddc385faa5a7dbe4bbcc24f5e943f4
Privezenov96 <91391340+Privezenov96@users.noreply.github.com>
2021-12-22 12:11:59 +0300

    20

---
+++ src/ru/mirea/task20/task20/DateBuilder.java
@@ -0,0 +1,19 @@
+package ru.mirea.task20.task20;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateBuilder extends DateTypeBuilder{
+    public Date build(String dateStr)
+    {
+        Date date;
+
+        long milliseconds = getMillisecondsFromDateStr(dateStr);
+        date = new Date(milliseconds);
+
+        return date;
+    }
+
+
+}
